119f494fad9cb3852a15203e17e3dac1746b2d58
rvpartap <83447089+Rvpartap@users.noreply.github.com>
2021-10-08 12:23:59 +0200

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+
+Module Program
+    Sub Main()
+        Dim numero As Integer
+        Dim scelta As String = "Si"
+        While scelta = "Si" Or scelta = "SI" Or scelta = "si"
+            Console.Write("Inserisci il numero della tabellina (da 1 a 10): ")
+            numero = Console.ReadLine()
+            For i = 1 To 10
+                Console.WriteLine(numero & " x " & i & " = " & (i * numero))
+            Next
+            Console.WriteLine("Vuoi Continuare? Si o No?")
+            scelta = Console.ReadLine()
+            Console.Clear()
+        End While
+    End Sub
+End Module
